39618002ad8c1b274d80f63e938c667c89de27ee
dev-yogesh <yogeshgupta279@gmail.com>
2022-04-03 09:44:12 +0530

    add: sorting algos - section 1

--- searchingSorting.js
+++ searchingSorting.js
@@ -103,3 +103,187 @@ const getLinearSearchUnsuccessfulComparisonCount = (inputArr, key) => {
   return count;
 };
 // console.log(getLinearSearchUnsuccessfulComparisonCount([2, 3, 4, 5, 8], 8));
+
+// 6th
+// Bubble Sort
+// TC - O(n^2)
+const swap = (i, j, array) => {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+};
+
+const bubbleSort = (numbers) => {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 1; j < numbers.length - i; j++) {
+      if (numbers[j - 1] > numbers[j]) {
+        //swap elements
+        swap(j - 1, j, numbers);
+      }
+    }
+  }
+  return numbers; // returning the final sorted array
+};
+// console.log(bubbleSort([1, 56, 3, 16, 2, 19]));
+
+// Optimized
+// const bubbleSort2 = (numbers) => {
+//   for (let i = 0; i < numbers.length; i++) {
+//     for (let j = 1; j < numbers.length - i; j++) {
+//       let swapCount = 0;
+//       if (numbers[j - 1] > numbers[j]) {
+//         //swap elements
+//         swap(j - 1, j, numbers);
+//         swapCount = 1;
+//       }
+//     }
+
+//     if (swapCount === 0) {
+//       break;
+//     }
+//   }
+
+//   return numbers; // returning the final sorted array
+// };
+
+// console.log(bubbleSort2([1, 2, 3, 4, 5, 6]));
+
+// 7th
+/**
+ * Write a bubble sort program that prints the number of swaps made after M number of
+ * iterations
+ */
+const totalBubbleSortSwaps = (array, M) => {
+  let size = array.length;
+  let temp = 0;
+  let totalSwaps = 0;
+  for (let i = 0; i < M; i++) {
+    for (let j = 1; j < size - i; j++) {
+      if (array[j - 1] < array[j]) {
+        // swap elements
+        temp = array[j - 1];
+        array[j - 1] = array[j];
+        array[j] = temp;
+        totalSwaps++;
+      }
+    }
+  }
+  return totalSwaps;
+};
+// console.log(totalBubbleSortSwaps([1, 2, 3, 4], 2));
+
+// 8th
+// Selection Sort
+// TC - O(n^2)
+const selectionSort = (arr) => {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < n; j++) if (arr[j] < arr[min]) min = j;
+
+    // Swap the found minimum element with the first element of the unsorted array
+    let temp = arr[min];
+    arr[min] = arr[i];
+    arr[i] = temp;
+  }
+
+  return arr;
+};
+// console.log(selectionSort([12, 5, 18, 3, 56]));
+
+// 9th
+// Insertion Sort
+// TC - O(n^2)
+const insertionSort = (arr) => {
+  let n = arr.length;
+  for (let i = 1; i < n; ++i) {
+    let j = i;
+
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      let temp = arr[j];
+      arr[j] = arr[j - 1];
+      arr[j - 1] = temp;
+      j = j - 1;
+    }
+  }
+
+  return arr;
+};
+// console.log(insertionSort([2, 7, 5, 21, 13]));
+
+// 10th
+const insertionSortDescending = (array) => {
+  let n = array.length;
+  for (let j = 1; j < n; j++) {
+    let key = array[j];
+    let i = j - 1;
+
+    while (i > -1 && array[i] < key) {
+      array[i + 1] = array[i];
+      i--;
+    }
+    array[i + 1] = key;
+  }
+
+  return array;
+};
+// console.log(insertionSortDescending([2, 7, 5, 21, 13]));
+
+// 11th
+const sortDescending = (lastNames) => {
+  let n = lastNames.length;
+  for (let j = 1; j < n; j++) {
+    let key = lastNames[j];
+    let i = j - 1;
+    while (i > -1 && lastNames[i].localeCompare(key) < 0) {
+      lastNames[i + 1] = lastNames[i];
+      i--;
+    }
+    lastNames[i + 1] = key;
+  }
+
+  return lastNames;
+};
+
+const sortAescending = (firstNames) => {
+  let n = firstNames.length;
+  for (let j = 1; j < n; j++) {
+    let key = firstNames[j];
+    let i = j - 1;
+    while (i > -1 && firstNames[i].localeCompare(key) >= 0) {
+      firstNames[i + 1] = firstNames[i];
+      i--;
+    }
+    firstNames[i + 1] = key;
+  }
+
+  return firstNames;
+};
+// const firstNames = ['logan', 'jean '];
+// const lastNames = ['gray', 'wolverine'];
+const firstNames = [
+  'janella',
+  'arianna',
+  'bob',
+  'sheryll',
+  'katelynn',
+  'alphonso',
+  'bertha',
+  'ivette',
+  'michiko',
+  'evelin',
+];
+const lastNames = [
+  'valene',
+  'krista',
+  'lyn',
+  'melony',
+  'laila',
+  'laree',
+  'raylene',
+  'lynwood',
+  'tim',
+  'keech',
+];
+// console.log(sortAescending(firstNames));
+// console.log(sortDescending(lastNames));
